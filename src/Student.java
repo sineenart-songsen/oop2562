2f5038e06fc83005d3711ce502f4bca8d3cf5e56
papercut <papercut@172.17.60.68>
2019-12-09 10:22:07 +0700

    chap1 9/12/2562

---
+++ src/Student.java
@@ -0,0 +1,37 @@
+public class Student {
+    protected String studentId;
+    protected String studentName;
+    protected String faculty;
+    protected String major;
+    
+    public void enrollment(){
+        System.out.println("enrollment");
+    }
+    public void payment(){
+        System.out.println("payment");        
+    }
+    public void addCourae(){
+        System.out.println("addCourae"); 
+    }
+    public void dropCourse(){
+        System.out.println("dropCourse"); 
+    
+    }
+    public void showData(){
+        System.out.println("ID : "+studentId);
+        System.out.println("Name : "+studentName);
+        System.out.println("Major: "+major);
+        System.out.println("Faculty : "+faculty);
+    }
+    
+    
+    
+    public static void main(String[] args)  {
+        Student sineenart = new Student();
+        System.out.println("Object = "+sineenart);
+        sineenart.studentId = "614234006";
+        sineenart.major = "cs";
+        sineenart.faculty = "Scinrce and ";
+    }
+    
+}
